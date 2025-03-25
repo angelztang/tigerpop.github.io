204d73bb0df44bbc6803a734eb7df9ca86cb9367
Angel Tang <angel.tang@princeton.edu>
2025-03-24 22:50:58 -0400

    heroku

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
-from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
 
-if __name__ == '__main__':
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
     app.run(debug=True)
+
