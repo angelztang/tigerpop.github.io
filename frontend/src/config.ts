49d4a8a8bbbff83f476c56c0a0f433ef6b536e26
Angel Tang <angel.tang@princeton.edu>
2025-03-25 23:09:20 -0400

    DEPLOY TO HEROKU

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://tigerpop-marketplace-1f7e8cd8acca.herokuapp.com/api'
+  ? 'https://tigerpop-marketplace-backend-76fa6fb8c8a2.herokuapp.com/api'
   : 'http://localhost:5001/api'; 
