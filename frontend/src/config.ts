65a150c434e9a9498541f217685651ed9d2b4c12
Angel Tang <angel.tang@princeton.edu>
2025-04-02 21:33:01 -0400

    Update API_URL to use production backend URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://tigerpop-marketplace-backend-76fa6fb8c8a2.herokuapp.com'
-  : 'http://localhost:8000'; 
+export const API_URL = 'https://tigerpop-marketplace-backend-76fa6fb8c8a2.herokuapp.com'; 
