2d501f23801404d1b73b81579a4b375fc2818543
Angel Tang <angel.tang@princeton.edu>
2025-05-09 02:46:28 -0400

    clean: yayyy

--- backend/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-import os
-
-app = create_app()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('FLASK_RUN_PORT', 8000))
-    app.run(host='127.0.0.1', port=port, debug=True) 
